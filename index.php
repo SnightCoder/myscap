3ef7a31a18b254ab0a3fa2348be3b40edf033eea
Nam Phạm <36730406+SnightCoder@users.noreply.github.com>
2023-12-28 21:47:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,14 +6,45 @@ $imp="/myscap/GDIPlus_Image1.jpg";
 ?>
 
 <?php
-$page = $_SERVER['PHP_SELF'];
-$sec = "1";
+//$page = $_SERVER['PHP_SELF'];
+//$sec = "1";
 ?>
+
 <html>
     <head>
-    <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
+	
+ <style>
+        * {
+            margin: 0;
+            padding: 0;    
+	}
+        .imgbox {
+            display: grid;
+            height: 100%;
+        }
+        .center-fit {
+            max-width: 100%;
+            max-height: 100vh;
+            margin: auto;
+	}
+    </style>
+   <!-- <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">-->
     </head>
     <body style="background-color:black;">
-<img src="<?php echo $imp; ?>" alt="scap" width="480" height="270" />
-    </body>
+<img id="imgC" src="<?php echo $imp; ?>" alt="scap" class="center-fit" loading="lazy" />
+<script>
+
+setInterval(refreshImage, 1000);
+
+function refreshImage(){
+	var timestamp = new Date().getTime();     
+     
+	var el = document.getElementById("imgC");     
+     
+	el.src = "/myscap/GDIPlus_Image1.jpg?t=" + timestamp;  
+}
+
+</script>
+
+</body>
 </html>
